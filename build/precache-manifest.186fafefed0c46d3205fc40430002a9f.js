43aed6dc2c7d44d74c8ff3680b0bc4d62b6a947e
wafaey <omarwafaey54@gmail.com>
2020-07-17 15:37:07 +0200

    add build folder

---
+++ build/precache-manifest.186fafefed0c46d3205fc40430002a9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aed4d447303b23d91f120e285a45960",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e310bf76411d001b1c85",
+    "url": "/static/css/main.f180bd69.chunk.css"
+  },
+  {
+    "revision": "674dee1f8e90f7f10a19",
+    "url": "/static/js/2.e47a8f59.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e47a8f59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e310bf76411d001b1c85",
+    "url": "/static/js/main.29920056.chunk.js"
+  },
+  {
+    "revision": "2fc8eda6d54797af01ea",
+    "url": "/static/js/runtime-main.a866bca2.js"
+  },
+  {
+    "revision": "01d33fb3670fc753c6d2b42a43645c73",
+    "url": "/static/media/Bank.01d33fb3.png"
+  },
+  {
+    "revision": "a6e8cd03d5273225964c250166591a17",
+    "url": "/static/media/Logowhite.a6e8cd03.png"
+  }
+]);
